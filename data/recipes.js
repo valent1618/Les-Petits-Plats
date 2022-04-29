88c9f147f529af02f204ff9f9c0eb0bea01a4550
valent1618 <valentinlemaire1618@gmail.com>
2022-04-29 12:33:41 +0200

    fix quantite to quantity

--- data/recipes.js
+++ data/recipes.js
@@ -20,7 +20,7 @@ export const recipes = [
             },
             {
                 "ingredient" : "Sucre",
-                "quantite" : 30,
+                "quantity" : 30,
                 "unit" : "grammes"
             },
             {
@@ -52,7 +52,7 @@ export const recipes = [
             },
             {
                 "ingredient" : "Carotte",
-                "quantite" : 1
+                "quantity" : 1
             },
             {
                 "ingredient" : "Citron Vert",
@@ -174,7 +174,7 @@ export const recipes = [
            {
                 "ingredient": "Moutarde de Dijon",
                 "quantity": 1,
-                "unite": "cuillères à soupe"
+                "unit": "cuillères à soupe"
            }
         ],
         "time": 45,
