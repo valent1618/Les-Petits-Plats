034117ad3dc73fc672aa2c09858ab6188202b5be
valent1618 <valentinlemaire1618@gmail.com>
2022-04-29 12:39:12 +0200

    add generic function

--- scripts/factory/filterList.js
+++ scripts/factory/filterList.js
@@ -9,25 +9,27 @@ export function fillFilterList() {
         switch(filter.getAttribute("data-filter")) {
             case "ingredients":
                 ingredients.forEach((ingredient) => {
-                    const li = document.createElement("li");
-                    li.textContent = ingredient;
-                    list.appendChild(li);
+                    list.appendChild(createOption(ingredient));
                 })
                 break;
             case "appliances":
                 appliances.forEach((appliance) => {
-                    const li = document.createElement("li");
-                    li.textContent = appliance;
-                    list.appendChild(li);
+                    list.appendChild(createOption(appliance));
                 })
                 break;
             case "ustensils":
                 ustensils.forEach((ustensil) => {
-                    const li = document.createElement("li");
-                    li.textContent = ustensil;
-                    list.appendChild(li);
+                    list.appendChild(createOption(ustensil));
                 })
                 break;
         }
     })
+}
+
+function createOption(option) {
+    const li = document.createElement("li");
+    li.className = "filter-option";
+    li.textContent = option;
+
+    return li;
 }
