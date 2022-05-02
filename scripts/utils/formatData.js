f1e4f0634024aa2c03aed0e0a5a52a2307de152a
valent1618 <valentinlemaire1618@gmail.com>
2022-05-02 19:13:35 +0200

    remove useless variable

--- scripts/utils/formatData.js
+++ scripts/utils/formatData.js
@@ -1,29 +1,32 @@
 // Functions for format data
 export function formatData(data) {
-    let formatData = data.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    formatData = formatData.toLowerCase();
-    formatData = formatData.charAt(0).toUpperCase() + formatData.slice(1);
+  let formatData = data.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  formatData = formatData.toLowerCase();
+  formatData = formatData.charAt(0).toUpperCase() + formatData.slice(1);
 
-    return formatData;
+  return formatData;
 }
 
 export function sortFilterData(data) {
-    // Remove occurrences
-    let sortFilterData = data.filter((d, i) => data.indexOf(d) === i);
+  // Remove occurrences
+  data.filter((d, i) => data.indexOf(d) === i);
 
-    // Sort alphabetically
-    sortFilterData.sort();
+  // Sort alphabetically
+  data.sort();
 
-    // Remove similar words
-    for(let i=1; i < sortFilterData.length; i++) {
-        const firstWordPreviousData = sortFilterData[i-1].split(' ')[0];
-        const firstWordCurrentData = sortFilterData[i].split(' ')[0];
-    
-        if(firstWordPreviousData === firstWordCurrentData || firstWordPreviousData + 's' === firstWordCurrentData) {
-            sortFilterData.splice(i-1, 2, firstWordPreviousData);
-            i--;
-        }
+  // Remove similar words
+  for (let i = 1; i < data.length; i++) {
+    const firstWordPreviousData = data[i - 1].split(" ")[0];
+    const firstWordCurrentData = data[i].split(" ")[0];
+
+    if (
+      firstWordPreviousData === firstWordCurrentData ||
+      firstWordPreviousData + "s" === firstWordCurrentData
+    ) {
+      data.splice(i - 1, 2, firstWordPreviousData);
+      i--;
     }
+  }
 
-    return sortFilterData;
+  return data;
 }
