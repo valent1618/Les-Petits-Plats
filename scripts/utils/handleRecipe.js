772aa2aed4efbc8c6b7a94e52317cc8712974f89
valent1618 <valentinlemaire1618@gmail.com>
2022-05-02 19:16:43 +0200

    add promise

--- scripts/utils/handleRecipe.js
+++ scripts/utils/handleRecipe.js
@@ -1,54 +1,60 @@
 import { recipes } from "../../data/recipes.js";
-import { formatData } from "./formatData.js"
+import { formatData } from "./formatData.js";
 
 export function handleRecipeByTags() {
-    const tags = document.querySelectorAll(".tag");
-    const options = document.querySelectorAll(".filter-option");
-    const recipesCard = document.querySelectorAll(".recipe");
+  const tags = document.querySelectorAll(".tag");
+  const recipesCard = document.querySelectorAll(".recipe");
 
+  return new Promise((res) => {
     recipesCard.forEach((recipeCard, i) => {
-        let remove = false;
+      let remove = false;
 
-        tags.forEach((tag) => {
-            const tagName = tag.querySelector("p").textContent;
-            let include = false;
+      tags.forEach((tag) => {
+        const tagName = tag.querySelector("p").textContent;
+        let include = false;
 
-            switch(tag.getAttribute("data-tagtype")){
-                case "ingredients":
-                    recipes[i].ingredients.forEach((ingredient) => {
-                        if(formatData(ingredient.ingredient).includes(tagName)){
-                            include = true;
-                        }
-                    });
-                    break;
-                case "appliances":
-                    if(formatData(recipes[i].appliance).includes(tagName)){
-                        include = true;
-                    }
-                    break;
-                case "ustensils":
-                    recipes[i].ustensils.forEach((ustensil) => {
-                        if(formatData(ustensil).includes(tagName)){
-                            include = true;
-                        }
-                    });
-                    break;
+        switch (tag.getAttribute("data-tagtype")) {
+          case "ingredients":
+            recipes[i].ingredients.forEach((ingredient) => {
+              if (formatData(ingredient.ingredient).includes(tagName)) {
+                include = true;
+              }
+            });
+            break;
+          case "appliances":
+            if (formatData(recipes[i].appliance).includes(tagName)) {
+              include = true;
             }
-            
-            if(!include) {
-                remove = true;
-            }
-        })
-        
-        recipeCard.setAttribute("data-remove", "true");
-        setTimeout(() => {
-            recipeCard.style.display = "none";
-            if(!remove) {
-                recipeCard.style.display = "flex";
-                setTimeout(() => {
-                    recipeCard.setAttribute("data-remove", "false");
-                }, 50);
-            }
-        }, 400); 
-    })
+            break;
+          case "ustensils":
+            recipes[i].ustensils.forEach((ustensil) => {
+              if (formatData(ustensil).includes(tagName)) {
+                include = true;
+              }
+            });
+            break;
+        }
+
+        if (!include) {
+          remove = true;
+        }
+      });
+
+      recipeCard.setAttribute("data-remove", "true");
+
+      setTimeout(() => {
+        recipeCard.style.display = "none";
+        if (!remove) {
+          recipeCard.style.display = "flex";
+          setTimeout(() => {
+            recipeCard.setAttribute("data-remove", "false");
+          });
+        }
+      }, 400);
+    });
+
+    setTimeout(() => {
+      res();
+    }, 500);
+  });
 }
