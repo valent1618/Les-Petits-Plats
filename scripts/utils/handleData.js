be61d6cd254fc91697417ac3fca6047dd58d73b5
valent1618 <valentinlemaire1618@gmail.com>
2022-05-02 19:15:35 +0200

    handle data with recipes in DOM and filter the tag

--- scripts/utils/handleData.js
+++ scripts/utils/handleData.js
@@ -1,27 +1,47 @@
 import { recipes } from "../../data/recipes.js";
 import { formatData, sortFilterData } from "./formatData.js";
 
-export let ingredients = [];
-export let appliances = [];
-export let ustensils = [];
+export function handleData() {
+  const recipesDOM = document.querySelectorAll(".recipe");
+  const tags = document.querySelectorAll(".tag");
 
-// For each recipe fill the arrays
-recipes.forEach((recipe) => {
-    // ingredients
-    recipe.ingredients.forEach((ingredient) => {
+  let ingredients = [];
+  let appliances = [];
+  let ustensils = [];
+
+  // For each recipe display fill the arrays
+  recipesDOM.forEach((recipe, i) => {
+    if (recipe.getAttribute("data-remove") === "false") {
+      // ingredients
+      recipes[i].ingredients.forEach((ingredient) => {
         ingredients.push(formatData(ingredient.ingredient));
-    })
+      });
 
-    // appliances
-    appliances.push(formatData(recipe.appliance));
+      // appliances
+      appliances.push(formatData(recipes[i].appliance));
 
-    // ustensils
-    recipe.ustensils.forEach((ustensil) => {
+      // ustensils
+      recipes[i].ustensils.forEach((ustensil) => {
         ustensils.push(formatData(ustensil));
-    })
-})
+      });
+    }
+  });
 
-// Sort and filter the arrays
-ingredients = sortFilterData(ingredients);
-appliances = sortFilterData(appliances);
-ustensils = sortFilterData(ustensils);
+  // Sort and filter the arrays
+  ingredients = sortFilterData(ingredients);
+  appliances = sortFilterData(appliances);
+  ustensils = sortFilterData(ustensils);
+
+  // Remove tags select of the list
+  if (tags.length > 0) {
+    tags.forEach((tag) => {
+      ingredients = ingredients.filter(
+        (data) => !data.includes(tag.textContent)
+      );
+      appliances = appliances.filter((data) => !data.includes(tag.textContent));
+      ustensils = ustensils.filter((data) => !data.includes(tag.textContent));
+    });
+  }
+
+  return { ingredients, appliances, ustensils };
+}
