fdb11555a9d17614ad900e20ad8d01ae6ee388cb
valent1618 <valentinlemaire1618@gmail.com>
2022-05-02 19:13:51 +0200

    prettier

--- scripts/components/filter.js
+++ scripts/components/filter.js
@@ -1,47 +1,51 @@
 const filters = document.querySelectorAll(".filter");
 
 export function listeningFilter() {
-    document.addEventListener("click", (e) => {
-        if(!e.target.closest(".filter")) {
-            filters.forEach((filter) => {
-                if(filter.getAttribute("data-expanded") === "true") {
-                    filter.setAttribute("data-expanded", "false");
-                    filter.querySelector("input").value = "";
-                    filter.querySelector("input").dispatchEvent(new Event("input"));
-                }
-            })
+  document.addEventListener("click", (e) => {
+    if (!e.target.closest(".filter")) {
+      filters.forEach((filter) => {
+        if (filter.getAttribute("data-expanded") === "true") {
+          filter.setAttribute("data-expanded", "false");
+          filter.querySelector("input").value = "";
+          filter.querySelector("input").dispatchEvent(new Event("input"));
         }
-    })
-    document.addEventListener("keydown", (e) => {
-        if(e.code === "Escape") {
-            document.body.click();
-            document.getElementById("search-recipe").focus();
+      });
+    }
+  });
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      document.body.click();
+      document.getElementById("search-recipe").focus();
+    }
+  });
+
+  filters.forEach((filter) => {
+    const input = filter.querySelector("input");
+
+    filter.addEventListener("click", (e) => {
+      if (!e.target.classList.contains("filter-option")) {
+        input.focus();
+      }
+    });
+
+    input.addEventListener("focus", () => {
+      document.body.click();
+      filter.setAttribute("data-expanded", "true");
+    });
+
+    input.addEventListener("input", (e) => {
+      const options = filter.querySelectorAll(".filter-option");
+      options.forEach((option) => {
+        if (
+          !option.textContent
+            .toLowerCase()
+            .includes(e.target.value.toLowerCase())
+        ) {
+          option.style.display = "none";
+        } else {
+          option.style.display = "block";
         }
-    })
-
-    filters.forEach((filter) => {
-        const input = filter.querySelector("input");
-        const options = filter.querySelectorAll(".filter-option");
-
-        filter.addEventListener("click", (e) => {
-            if(!e.target.classList.contains("filter-option")){
-                input.focus();
-            }
-        })
-
-        input.addEventListener("focus", () => {
-            document.body.click();
-            filter.setAttribute("data-expanded", "true");
-        })
-
-        input.addEventListener("input", (e) => {
-            options.forEach((option) => {
-                if(!option.textContent.toLowerCase().includes(e.target.value.toLowerCase())) {
-                    option.style.display = "none";
-                } else {
-                    option.style.display = "block"
-                }
-            })
-        })
-    })
+      });
+    });
+  });
 }
