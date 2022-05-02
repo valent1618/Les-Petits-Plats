196e8a177b7e27f6136fe7117ace78a2ccaf5519
valent1618 <valentinlemaire1618@gmail.com>
2022-05-02 19:17:02 +0200

    handle filter list when tag is select

--- scripts/components/tags.js
+++ scripts/components/tags.js
@@ -1,37 +1,45 @@
 import { createTag } from "../factory/tag.js";
 import { handleRecipeByTags } from "../utils/handleRecipe.js";
+import { handleFilterList } from "../utils/handleFilterList.js";
 
 const tagsContainer = document.querySelector(".tags-container");
 
 export function handleTags() {
-    const options = document.querySelectorAll(".filter-option");
-    
-    options.forEach((option) => {
-        // Add tag
-        option.addEventListener("click", () => {
-            option.style.display = "none";
+  const options = document.querySelectorAll(".filter-option");
 
-            if(!tagsContainer.classList.contains("pt-4")) {
-                tagsContainer.classList.add("pt-4");
-            }
+  options.forEach((option) => {
+    // Add tag
+    option.addEventListener("click", () => {
+      if (!tagsContainer.classList.contains("pt-4")) {
+        tagsContainer.classList.add("pt-4");
+      }
 
-            const typeOption = option.parentElement.parentElement.getAttribute("data-filter");
-            tagsContainer.prepend(createTag(option.textContent, typeOption));
+      const optionType =
+        option.parentElement.parentElement.getAttribute("data-filter");
+      const optionName = option.textContent;
+      tagsContainer.prepend(createTag(optionName, optionType));
 
-            handleRecipeByTags();
+      options.forEach((option) => {
+        option.setAttribute("data-remove", "true");
+      });
 
-            // Remove tag
-            tagsContainer.children[0].addEventListener("click", (e) => {
-                e.target.closest(".tag").setAttribute("data-remove", "true");
-                setTimeout(() => {
-                    tagsContainer.removeChild(e.target.closest(".tag"));
-                    if(tagsContainer.childElementCount === 0) {
-                        tagsContainer.classList.remove("pt-4");
-                    }
-                    handleRecipeByTags();
-                }, 200);
-                option.style.display = "block";
-            })
-        });
-    })
+      handleRecipeByTags()
+        .then(() => handleFilterList())
+        .then(() => handleTags());
+
+      // Remove tag
+      tagsContainer.children[0].addEventListener("click", (e) => {
+        e.target.closest(".tag").setAttribute("data-remove", "true");
+        setTimeout(() => {
+          tagsContainer.removeChild(e.target.closest(".tag"));
+          if (tagsContainer.childElementCount === 0) {
+            tagsContainer.classList.remove("pt-4");
+          }
+          handleRecipeByTags()
+            .then(() => handleFilterList())
+            .then(() => handleTags());
+        }, 200);
+      });
+    });
+  });
 }
