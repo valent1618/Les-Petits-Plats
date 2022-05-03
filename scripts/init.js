6eec502a90b790450102b77762a1653ce73675e2
valent1618 <valentinlemaire1618@gmail.com>
2022-05-03 19:06:55 +0200

    disable forms

--- scripts/init.js
+++ scripts/init.js
@@ -1,9 +1,11 @@
 import { fillRecipesContainer } from "./factory/recipeCard.js";
 import { handleFilterList, listeningFilter } from "./components/filter.js";
+import { disableForm } from "./components/forms.js";
 
 // Factory
 fillRecipesContainer();
 handleFilterList();
 
 // Events
+disableForm();
 listeningFilter();
