25c049e0a8f6a2120b5223bd747548b19a7152a2
valent1618 <valentinlemaire1618@gmail.com>
2022-05-06 20:17:20 +0200

    add handle recipe by search

--- scripts/utils/handleRecipe.js
+++ scripts/utils/handleRecipe.js
@@ -1,8 +1,114 @@
 import { recipes } from "../../data/recipes.js";
 import { formatData } from "./formatData.js";
+import { handleNoRecipeMessage } from "../components/noRecipeMessage.js";
 import { handleFilterList } from "../components/filter.js";
 
+export function handleRecipeBySearch(search) {
+  const recipesCard = document.querySelectorAll(".recipe");
+
+  search = formatData(search);
+
+  let titles = [];
+  let allRemoved = true;
+  for (let i = 0; i < recipesCard.length; i++) {
+    if (recipesCard[i].getAttribute("data-remove") === "false") {
+      if (search.length > 0) {
+        let remove = false;
+
+        let title = formatData(recipesCard[i].querySelector("h2").textContent);
+        titles.push(title);
+
+        let description = formatData(
+          recipesCard[i].querySelector(".card-description").textContent
+        );
+
+        let ingredients = [];
+        for (let j = 0; j < recipes[i].ingredients.length; j++) {
+          ingredients.push(formatData(recipes[i].ingredients[j].ingredient));
+        }
+
+        // Test if search is include
+        let include = false;
+
+        let stop = false;
+        // if it's include or if we check all the array
+        // stop searching
+        while (!stop) {
+          // title
+          for (let j = 0; j < title.length; j++) {
+            for (let k = 0; k < search.length; k++) {
+              if (title[j] === search[k]) {
+                if (k === search.length - 1) {
+                  include = true;
+                  stop = true;
+                }
+                j++;
+              } else {
+                k = search.length;
+              }
+            }
+          }
+
+          // description
+          for (let j = 0; j < description.length; j++) {
+            for (let k = 0; k < search.length; k++) {
+              if (description[j] === search[k]) {
+                if (k === search.length - 1) {
+                  include = true;
+                  stop = true;
+                }
+                j++;
+              } else {
+                k = search.length;
+              }
+            }
+          }
+
+          // ingredients
+          for (let j = 0; j < ingredients.length; j++) {
+            for (let k = 0; k < ingredients[j].length; k++) {
+              for (let l = 0; l < search.length; l++) {
+                if (ingredients[j][k] === search[l]) {
+                  if (l === search.length - 1) {
+                    include = true;
+                    stop = true;
+                  }
+                  k++;
+                } else {
+                  l = search.length;
+                }
+              }
+            }
+          }
+
+          stop = true;
+        }
+
+        if (!include) {
+          remove = true;
+        }
+
+        if (remove) {
+          recipesCard[i].style.display = "none";
+        } else {
+          allRemoved = false;
+          recipesCard[i].removeAttribute("style");
+        }
+      } else {
+        allRemoved = false;
+        recipesCard[i].removeAttribute("style");
+      }
+    }
+  }
+
+  handleNoRecipeMessage(allRemoved, titles);
+
+  // Handle options in the filter lists
+  handleFilterList();
+}
+
 export function handleRecipeByTags() {
+  const inputSearch = document.getElementById("search-recipe");
   const tags = document.querySelectorAll(".tag");
   const recipesCard = document.querySelectorAll(".recipe");
 
@@ -49,6 +155,10 @@ export function handleRecipeByTags() {
       recipeCard.setAttribute("data-remove", "false");
     }
   });
+
+  if (inputSearch.value.length > 2) {
+    handleRecipeBySearch(inputSearch.value);
+  }
 
   // Handle options in the filter lists
   handleFilterList();
